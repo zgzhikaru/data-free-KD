2b823756dc813ea9dd1b43d1575583445b581cea
autodl <1245607800@qq.com>
2024-12-11 10:21:06 +0800

    添加新的运行脚本以支持AE Mosaic方法，配置CIFAR-10数据集和相关训练参数

---
+++ scripts/ae_mosaic/run_aemosaic.sh
@@ -0,0 +1,6 @@
+python ood_kd.py --method ae_mosaic --dataset cifar10 --transfer_set cifar10 \
+ --batch_size 256  --include_raw --shared_normalizer True\
+ --teacher wrn40_2 --student wrn16_1 --lr 0.1 --recon 1 \
+ --kd_steps 5 --ep_steps 400 --g_steps 1 --lr_g 1e-3 --epochs 200 \
+ --lr_decay_milestones 25,30,35 --adv 1  --entropy 1 --local 1\
+ --T 20 --act 0 --balance 10 --T 1 --gpu 0 \
