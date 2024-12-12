554e6d3ad090fc95dafb158386b35c1b13c0ec8b
autodl <1245607800@qq.com>
2024-12-12 10:45:41 +0800

    更新AE Mosaic和CMI脚本，调整数据集和批量大小，添加fp16支持，修改教师和学生模型参数

--- scripts/mosaic/run_mosaic.sh
+++ scripts/mosaic/run_mosaic.sh
@@ -1,14 +1,14 @@
-# python ood_kd.py --method mosaic --dataset cifar100 --transfer_set cifar10 \
-#  --batch_size 256  --include_raw --shared_normalizer True\
-#  --teacher wrn40_2 --student wrn16_1 --lr 0.1 \
-#  --kd_steps 5 --ep_steps 400 --g_steps 1 --lr_g 1e-3 --epochs 200 \
-#  --lr_decay_milestones 25,30,35 --adv 1  --entropy 1 --local 1\
-#  --T 20 --act 0 --balance 10 --T 1 --gpu 0 \
-
 python ood_kd.py --method mosaic --dataset cifar100 --transfer_set cifar10 \
  --batch_size 256  --include_raw --shared_normalizer True\
- --teacher resnet34 --student resnet18 --lr 0.1 \
+ --teacher wrn40_2 --student wrn16_1 --lr 0.1 \
  --kd_steps 5 --ep_steps 400 --g_steps 1 --lr_g 1e-3 --epochs 200 \
  --lr_decay_milestones 25,30,35 --adv 1  --entropy 1 --local 1\
- --T 20 --act 0 --balance 10 --T 1 --gpu 1 \
- --resume /home/CMI/CMI/checkpoints/ood_kd-mosaic/cifar100-resnet34-resnet18.pth
+ --T 20 --act 0 --balance 10 --T 1 --gpu 0 --fp16
+
+
+python ood_kd.py --method mosaic --dataset cifar10 --transfer_set cifar100 \
+ --batch_size 1024  --include_raw --shared_normalizer True\
+ --teacher wrn40_2 --student wrn16_1 --lr 0.1 \
+ --kd_steps 5 --ep_steps 400 --g_steps 1 --lr_g 1e-3 --epochs 200 \
+ --lr_decay_milestones 25,30,35 --adv 1  --entropy 1 --local 1\
+ --T 20 --act 0 --balance 10 --T 1 --gpu 0 --fp16
